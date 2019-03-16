b1b4028b78cfb4866e66591e953963ea2f4d94ef
Pol Gasull <pol@innroute.com>
2019-03-16 20:00:01 +0100

    Added uploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190316185013) do
+ActiveRecord::Schema.define(version: 20190316185943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20190316185013) do
     t.string "apply_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar"
   end
 
   create_table "users", force: :cascade do |t|
