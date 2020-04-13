ec3b51335c787e4e9c97b726c39da2a0f5c56d28
polgasull <pgasulln@gmail.com>
2020-04-13 21:49:16 +0200

    Linkedin sign up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200407140326) do
+ActiveRecord::Schema.define(version: 20200413190754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,8 @@ ActiveRecord::Schema.define(version: 20200407140326) do
     t.string "card_exp_year"
     t.datetime "expires_at"
     t.boolean "admin"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
