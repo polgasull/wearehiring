1c342f2222810ad3de46310901ab1edbfd3e0962
polgasull <pgasulln@gmail.com>
2020-04-16 15:55:56 +0200

    vendor compiled assets

---
+++ public/assets/global/global-8322b1b7cf1a4ab21ea8f65eb471e075abb686e269aac5b5fe7fc813ecce223d.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  let dropdown = document.querySelector('.dropdown');
+  dropdown.addEventListener('click', function(event) {
+      event.stopPropagation();
+      dropdown.classList.toggle('is-active');
+  });
+});
