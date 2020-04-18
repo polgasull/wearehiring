0535f14628f68968aacec2772a7d8e8281ba199e
polgasull <pgasulln@gmail.com>
2020-04-18 11:36:22 +0200

    Fixed sidenav and header position and bg color

--- app/assets/javascripts/global/global.js
+++ app/assets/javascripts/global/global.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
-  let dropdown = document.querySelector('.dropdown');
-  dropdown.addEventListener('click', function(event) {
-      event.stopPropagation();
-      dropdown.classList.toggle('is-active');
-  });
+  if ($("#profileDropdown").length > 0) { 
+    let dropdown = document.querySelector('.dropdown');
+    dropdown.addEventListener('click', function(event) {
+        event.stopPropagation();
+        dropdown.classList.toggle('is-active');
+    });  
+  }
 });
