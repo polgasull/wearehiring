3ff04a0cf8d2485454c37b7fc90ef688a86220b5
polgasull <pgasulln@gmail.com>
2020-04-27 12:07:04 +0200

    Resize image with carrierwave

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -31,13 +31,13 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [200, 200]
+    process resize_to_fill: [200, 200]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
-    %w(jpg jpeg gif png)
+    %w(jpg jpeg gif png svg)
   end
 
   # Override the filename of the uploaded files:
