4d6036641a7a2b65147e4549560cc48fdd0bb9ed
polgasull <pgasulln@gmail.com>
2020-06-11 18:17:37 +0200

    Added pagination in admin users list

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -5,7 +5,7 @@ module Admins
     before_action :set_user, only: [:update, :inscriptions]
 
     def index
-      @users = User.all.order('created_at DESC')
+      @users = User.all.order('created_at DESC').page(params[:page]).per(50)
       @companies = User.where(user_type: 2).count
       @candidates = User.where(user_type: 1).count
     end 
