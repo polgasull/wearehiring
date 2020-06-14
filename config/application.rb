2ac9b670df285086045c1ad8bc9c3b216fc4bbee
polgasull <pgasulln@gmail.com>
2020-06-14 21:22:15 +0200

    Deflacker and logo image

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module JobApp
     config.assets.initialize_on_precompile = false
     config.serve_static_assets = true
     config.exceptions_app = self.routes
+    config.middleware.use Rack::Deflater
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
