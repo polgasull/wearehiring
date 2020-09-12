762d01ad516e251a6e94e076fafef67ecaf448c0
polgasull <pgasulln@gmail.com>
2020-09-13 00:43:19 +0200

    Check if user profile is completed

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,5 +4,9 @@ module UsersHelper
 
   def user_inscribed_to(job)  
     current_user.inscriptions.any? {|u| u[:job_id] == job.id}
-  end 
+  end
+
+  def profile_is_completed(current_user)
+    %w( picture_url profile_url phone current_position ).all? { |attr| current_user[attr].present? }
+  end
 end
