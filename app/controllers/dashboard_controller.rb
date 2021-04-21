c4e5ecd9e4f5cf7a4b3b7f4c2907304c0adf0eb3
polgasull <pgasulln@gmail.com>
2021-04-21 16:46:18 +0200

    Add groupdate gem for admin dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,8 +4,9 @@ class DashboardController < ApplicationController
   def index
     @jobs = Job.active.order('created_at DESC').take(10);
     @jobs_count = Job.active.take(10).count
-    candidate_variables(current_user)
-    company_variables(current_user) 
+    candidate_variables(current_user) if current_user.is_candidate?
+    company_variables(current_user) if current_user.is_company? || current_user.is_ambassador? 
+    admin_variables if current_user.is_admin?
   end
 
   private 
@@ -23,5 +24,14 @@ class DashboardController < ApplicationController
     @candidate_inscriptions = user.inscriptions.all
     @in_process_count = user.inscriptions.where(status: [1]).count
     @finalists_count = user.inscriptions.where(status: [2]).count
+  end
+
+  def admin_variables
+    @candidates_by_date = User.where(user_type: 1).group_by_day(:created_at, last: 20, reverse: true).count
+    @candidates_by_week = User.where(user_type: 1).group_by_week(:created_at, last: 20, reverse: true).count
+    @candidates_by_month = User.where(user_type: 1).group_by_month(:created_at, format: "%b %Y", last: 20, reverse: true).count
+    @companies_by_date = User.where(user_type: 2).group_by_day(:created_at, last: 20, reverse: true).count
+    @companies_by_week = User.where(user_type: 2).group_by_week(:created_at, last: 20, reverse: true).count
+    @companies_by_month = User.where(user_type: 2).group_by_month(:created_at, format: "%b %Y", last: 20, reverse: true).count
   end
 end
