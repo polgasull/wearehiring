3722ad2c3fa9b7b7cb80b6bdafdb6542cd9a4821
polgasull <pgasulln@gmail.com>
2021-11-30 01:21:23 +0100

    Safer for sending mails if production

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   has_many :jobs
   has_many :inscriptions
   mount_uploader :picture_url, AvatarUploader
-  after_create :send_welcome_mail
+  after_create :send_welcome_mail if Rails.env.production?
 
   %w[candidate company admin ambassador].each do |user_type_name|
     define_method "is_#{user_type_name}?" do
