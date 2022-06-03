fb0656c512d785a26b3806c5d46563df94efa8e5
polgasull <pgasulln@gmail.com>
2022-06-04 00:32:05 +0200

    Add assets

---
+++ public/dev-assets/helpers/image_loader-d247d64246e1699f7e23974c0683ccc8fb189cfa58e4d22002816a13ace1776c.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+
+  var instrumentImage = document.querySelector('.load-image');
+
+  function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
+
+    // Loop through the FileList and render image files as thumbnails.
+    for (var i = 0, f; f = files[i]; i++) {
+
+      // Only process image files.
+      if (!f.type.match('image.*')) {
+        continue;
+      }
+
+      var reader = new FileReader();
+
+      // Closure to capture the file information.
+      reader.onload = (function(theFile) {
+        return function(e) {
+          // Render thumbnail.
+          var newDiv = document.createElement('div');
+          var submitButton = document.createElement('div')
+          newDiv.classList.add('rounded-image', 'size-150', 'mauto');
+          newDiv.innerHTML = ['<img class="image-preview-thumb border-light" src="', e.target.result,
+            '" title="', escape(theFile.name), '"/>'
+          ].join('');
+          if (window.location.href.indexOf("users") > -1) {
+            submitButton.innerHTML = ['<button class="button is-info mt3" type="submit">Guardar</button>']
+          }
+          document.getElementById('list').replaceWith(newDiv, submitButton);
+        };
+      })(f);
+      // Read in the image file as a data URL.
+      reader.readAsDataURL(f);
+    }
+  }
+
+  if (instrumentImage) {
+    instrumentImage.addEventListener('change', handleFileSelect, false);
+  }
+  
+});
