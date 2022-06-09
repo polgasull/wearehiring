fc0f03fa2899eeae8d7ee8fa72ee39f9d45a01c6
polgasull <pgasulln@gmail.com>
2022-06-09 20:27:51 +0200

    Install invisible captcha for devise

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,7 @@
 class RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
+  invisible_captcha only: [:create]
 
   def create
     super do
