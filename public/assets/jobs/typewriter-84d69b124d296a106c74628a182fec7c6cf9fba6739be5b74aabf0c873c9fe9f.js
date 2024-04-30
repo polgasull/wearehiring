6c515e96699a43772c52f70a6989b81d470f5307
polgasull <pgasulln@gmail.com>
2024-04-30 17:37:22 +0200

    Vendor assets

---
+++ public/assets/jobs/typewriter-84d69b124d296a106c74628a182fec7c6cf9fba6739be5b74aabf0c873c9fe9f.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+  // Add something to given element placeholder
+  function addToPlaceholder(toAdd, el) {
+    el.attr('placeholder', el.attr('placeholder') + toAdd);
+    // Delay between symbols "typing" 
+    return new Promise(resolve => setTimeout(resolve, 100));
+  }
+
+  // Cleare placeholder attribute in given element
+  function clearPlaceholder(el) {
+    el.attr("placeholder", "");
+  }
+
+  // Print one phrase
+  function printPhrase(phrase, el) {
+    return new Promise(resolve => {
+        // Clear placeholder before typing next phrase
+        clearPlaceholder(el);
+        let letters = phrase.split('');
+        // For each letter in phrase
+        letters.reduce(
+            (promise, letter, index) => promise.then(_ => {
+                // Resolve promise when all letters are typed
+                if (index === letters.length - 1) {
+                    // Delay before start next phrase "typing"
+                    setTimeout(resolve, 1000);
+                }
+                return addToPlaceholder(letter, el);
+            }),
+            Promise.resolve()
+        );
+    });
+  } 
+
+  // Print given phrases to element
+  function printPhrases(phrases, el) {
+    // For each phrase
+    // wait for phrase to be typed
+    // before start typing next
+    phrases.reduce(
+        (promise, phrase, index) => promise.then(_ => 
+          { if (index === phrases.length - 1) {
+            setTimeout(() => {
+              run()
+            }, 3000);
+          }
+          return printPhrase(phrase, el)
+        }), 
+        Promise.resolve()
+    );
+  }
+
+  // Start typing
+  function run() {
+    let phrases = [
+        "Blockchain Engineer",
+        "Web3",
+        "UX Designer",
+        "Ruby on rails",
+        "Remote",
+        "Javascript",
+        "AWS",
+        "Product Owner"
+    ];
+
+    printPhrases(phrases, $('#search'));
+  }
+  
+  run();
+});
