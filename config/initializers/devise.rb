d0b04d7104fc2f21ec126b9ee257d9cf7d4f4d94
polgasull <pgasulln@gmail.com>
2024-10-09 15:52:50 +0200

    Renew devise secret key

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,7 +8,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  config.secret_key = '88541be69a393373d18d8de2fadc5ee9f65579b0898163838f3de51642a39be8ee559f42d0c8ff4e6046b70d9d123fd35efeddbc2e5cb7ce18440d3b8bc47722'
+  config.secret_key = ENV['DEVISE_SECRET_KEY']
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
